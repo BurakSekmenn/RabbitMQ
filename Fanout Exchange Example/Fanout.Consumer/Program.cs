6082aafc98360d67169216d53baf692c9e2977e1
Burak Sekmen <burakskmn07@gmail.com>
2024-08-09 16:07:42 +0300

    Fanout Exchange Example

---
+++ Fanout Exchange Example/Fanout.Consumer/Program.cs
@@ -0,0 +1,43 @@
+﻿// Fanout Exchange -> Mesajların, bu exchange'e bind olmuş olan tüm kuyruklara gönderilmesini sağlar. Publisher mesajların gönderildiği kuyruk isimlere dikkate almaz ve mesajlara tüm kuyruklara gönderir
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+//bağlantı oluşturma
+ConnectionFactory factory = new();
+factory.Uri = new("YouURl");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare(
+    exchange: "fanout-exchange-example",
+    type:ExchangeType.Fanout
+    );
+
+Console.WriteLine("Kuyruk Adını Giriniz:");
+string queueName = Console.ReadLine();
+
+channel.QueueDeclare(queue:queueName,
+    exclusive:false);
+
+channel.QueueBind(
+    queue:queueName,
+    exchange: "fanout-exchange-example",
+    routingKey:string.Empty
+    );
+
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(queue:queueName,
+    autoAck:true,consumer:consumer);
+
+consumer.Received += (sender, e) =>
+{
+    string message = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine(message);
+};
+
+
+
+Console.Read();
---
+++ Fanout Exchange Example/Fanout.Publisher/Program.cs
@@ -0,0 +1,29 @@
+﻿//// Fanout Exchange -> Mesajların, bu exchange'e bind olmuş olan tüm kuyruklara gönderilmesini sağlar. Publisher mesajların gönderildiği kuyruk isimlere dikkate almaz ve mesajlara tüm kuyruklara gönderir
+///
+using RabbitMQ.Client;
+using System.Text;
+
+//bağlantı oluşturma
+ConnectionFactory factory = new();
+factory.Uri = new("YouURl");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange:"fanout-exchange-example",
+    type:ExchangeType.Fanout);
+
+for (int i = 1; i < 100; i++)
+{
+    await Task.Delay(200);
+    byte[] message = Encoding.UTF8.GetBytes($"Merhaba {i}");
+    channel.BasicPublish(
+        exchange: "fanout-exchange-example",
+        routingKey:string.Empty,
+        body:message
+        );// Fanout rotuing key boş olmalı çünkü fıtratı gereği böyle
+}
+
+
+Console.Read();
+
