650f463ced7297e014cefa2c1ea60496769e85af
Burak Sekmen <burakskmn07@gmail.com>
2024-08-14 01:19:13 +0300

    Topic Exchange Example

---
+++ Topic Exchange Example/Topic.Customer/Program.cs
@@ -0,0 +1,43 @@
+﻿// routing key'leri kullanarak mesajları kuyruklara yönlendirmek için kullanılan bir exchange türüdür.Bu exchange ile routing key'in bir ksımına/formatına/yapısına keylere göre kuyruklara mesaj gönderiliri.
+// kuyruklar da routing key'e göre ebu exchange'e abone olabilirler ve sadece ilgil routing key'e göre gönderilen mesajları alabilirler.
+
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+//bağlantı oluşturma
+ConnectionFactory factory = new();
+factory.Uri = new("YouURl");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare(
+    exchange: "topic-exchange-example",
+    type: ExchangeType.Topic);
+
+Console.WriteLine("Dinlenecek Topic Formatını Belirtiniz:");
+string topic = Console.ReadLine();
+
+
+string quename = channel.QueueDeclare().QueueName; // Query ismi aldım 
+channel.QueueBind(queue:quename,
+    exchange: "topic-exchange-example",
+    routingKey:topic);
+
+
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(
+    
+    queue:quename,
+    autoAck:true,
+    consumer);
+
+consumer.Received += (sender, e) =>
+{
+    string message = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine(message);
+};
+
+Console.Read();
---
+++ Topic Exchange Example/Topic.Publisher/Program.cs
@@ -0,0 +1,36 @@
+﻿//// routing key'leri kullanarak mesajları kuyruklara yönlendirmek için kullanılan bir exchange türüdür.Bu exchange ile routing key'in bir ksımına/formatına/yapısına keylere göre kuyruklara mesaj gönderiliri.
+// kuyruklar da routing key'e göre ebu exchange'e abone olabilirler ve sadece ilgil routing key'e göre gönderilen mesajları alabilirler.
+
+using RabbitMQ.Client;
+using System.Text;
+
+//bağlantı oluşturma
+ConnectionFactory factory = new();
+factory.Uri = new("YouURl");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare(
+    exchange:"topic-exchange-example",
+    type:ExchangeType.Topic);
+for (int i = 0; i < 100; i++)
+{
+    await Task.Delay(200);
+    byte[] message = Encoding.UTF8.GetBytes($"Message {i}");
+    Console.WriteLine("Mesjaın Gönderilecek Topic Formatını Belirtiniz : ");
+    string topic = Console.ReadLine();
+    channel.BasicPublish(
+               exchange: "topic-exchange-example",
+                      routingKey: topic,
+                                    body: message);
+}
+
+Console.Read();
+
+
+// Rabbit Toxic Gödnderirken
+// # => 0 veya daha fazla kelimeyi temsil eder.
+// * => 1 kelimeyi temsil eder.
+// https://www.gencayyildiz.com/blog/rabbitmq-topic-exchange/ tam içerği burada var.
+// routing key'leri kullanarak mesajları kuyruklara yönlendirmek için kullanılan bir exchange türüdür.Bu exchange ile routing key'in bir ksımına/formatına/yapısına keylere göre kuyruklara mesaj gönderiliri.
