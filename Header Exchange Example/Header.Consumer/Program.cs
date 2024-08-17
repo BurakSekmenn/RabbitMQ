fd0b1774cb0f8808f9ed221c4cd00d8c3e0b63d9
Burak Sekmen <burakskmn07@gmail.com>
2024-08-17 20:47:46 +0300

    Header Exchange Example

---
+++ Header Exchange Example/Header.Consumer/Program.cs
@@ -0,0 +1,51 @@
+﻿// Routing key yerine header'ları kullanarak mesajları kuyruklara yönelendirmek için kullanılan exchange'dir.
+
+// x-match : iligli queue'nun mesajı hangi davranisla lacağını kararini veren bir keydir.
+// all : tüm header'lar eşleşmeli
+// any : en az bir header eşleşmeli
+// none : hiç bir header eşleşmemeli
+
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+//bağlantı oluşturma
+ConnectionFactory factory = new();
+factory.Uri = new("YouURl");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare(
+       exchange:"header-exchange", 
+          type: ExchangeType.Headers);
+
+Console.WriteLine("Lütfen Header Valu'sunu Giriniz : ");
+string value = Console.ReadLine();
+
+string queueName=channel.QueueDeclare().QueueName;
+
+channel.QueueBind(
+    queue: queueName,
+    exchange: "header-exchange",
+    routingKey: string.Empty,
+    arguments: new Dictionary<string, object>
+    {
+        ["x-match"] = "all",
+        ["no"] = value,
+        ["For Degeri"] = 1
+    }
+    );
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+
+consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+{
+    string message = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine(message);
+};
+
+
+
+Console.Read();
---
+++ Header Exchange Example/Header.Publisher/Program.cs
@@ -0,0 +1,48 @@
+﻿// Routing key yerine header'ları kullanarak mesajları kuyruklara yönelendirmek için kullanılan exchange'dir.
+
+// x-match : iligli queue'nun mesajı hangi davranisla lacağını kararini veren bir keydir.
+// all : tüm header'lar eşleşmeli
+// any : en az bir header eşleşmeli
+// none : hiç bir header eşleşmemeli
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+//bağlantı oluşturma
+ConnectionFactory factory = new();
+factory.Uri = new("YouURl");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare(
+    exchange:"header-exchange", 
+    type: ExchangeType.Headers);
+
+for (int i = 0; i < 100; i++)
+{
+    await Task.Delay(200);
+    byte[] message = Encoding.UTF8.GetBytes($"Merhaba {i}");
+    Console.WriteLine("Lütfen Header Value'sunu Giriniz:");
+    string value = Console.ReadLine();
+
+    IBasicProperties basicProperties= channel.CreateBasicProperties();
+    basicProperties.Headers = new Dictionary<string, object>
+    {
+        ["no"] = value,
+        ["For Degeri"] = i
+    };
+
+    channel.BasicPublish(
+        exchange: "header-exchange",
+        routingKey: string.Empty,
+        body:message,
+        basicProperties: basicProperties
+        );
+}
+
+
+
+Console.Read();
+
