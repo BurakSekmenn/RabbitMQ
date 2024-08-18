2a2f4ab8cb8b537b389b164c563e528b57f3960a
Burak Sekmen <burakskmn07@gmail.com>
2024-08-18 21:23:24 +0300

    Work Queue Example

---
+++ Work Queue Example/WorkQueue.Consumer/Program.cs
@@ -0,0 +1,35 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+//bağlantı oluşturma
+ConnectionFactory factory = new();
+factory.Uri = new("");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+string queueName = "example-work-queue";
+
+channel.QueueDeclare(queue: queueName,
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false);
+
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(queue: queueName,
+                        autoAck: true,
+                        consumer: consumer);
+
+channel.BasicQos(
+    prefetchCount: 1,
+    prefetchSize: 0,
+    global: false); // 1 tane mesajı alıp işleyecek şekilde ayarlandı. WOrk queue mantığı
+
+consumer.Received += (sender, eventArgs) =>
+{
+    byte[] body = eventArgs.Body.ToArray();
+    string message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"Received: {message}");
+};
+Console.Read();
---
+++ Work Queue Example/WorkQueue.Publisher/Program.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+//bağlantı oluşturma
+ConnectionFactory factory = new();
+factory.Uri = new("");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+string queueName = "example-work-queue";
+
+channel.QueueDeclare(queue: queueName,
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false);
+
+for (int i = 0; i < 100; i++)
+{
+    await Task.Delay(200);
+    byte[] body = Encoding.UTF8.GetBytes($"Message {i}");
+
+    channel.BasicPublish(
+     exchange: string.Empty,
+     routingKey: queueName,
+     body: body);
+
+}
+
+
+
+Console.Read();
