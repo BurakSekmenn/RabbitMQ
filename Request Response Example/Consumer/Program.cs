7b894ea3d820c725871f8c3caff58f7b3d2baefc
Burak Sekmen <burakskmn07@gmail.com>
2024-08-18 22:08:53 +0300

    Request/Response pattern

---
+++ Request Response Example/Consumer/Program.cs
@@ -0,0 +1,45 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+//bağlantı oluşturma
+ConnectionFactory factory = new();
+factory.Uri = new("");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+string requestQueueName = "example-request-response-queue";
+channel.QueueDeclare(
+    queue: requestQueueName,
+    durable: false,
+    exclusive: false,
+    autoDelete: false
+    );
+
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(
+            queue: requestQueueName,
+            autoAck: true,
+            consumer: consumer);
+
+consumer.Received += (sender, eventArgs) =>
+{
+    string message = Encoding.UTF8.GetString(eventArgs.Body.ToArray());
+    Console.WriteLine(message);
+    //.. işlemleri yaptın vs
+    byte[] responseMessage = Encoding.UTF8.GetBytes("İşlem Tamamlandı :" + message);
+    IBasicProperties properties = channel.CreateBasicProperties();
+    properties.CorrelationId = eventArgs.BasicProperties.CorrelationId;// correlationId'yi ekliyoruz.// Göndereceğimiz mesajın hangi kuyruğa gönderileceğini belirtiyoruz.
+    channel.BasicPublish(
+        exchange: string.Empty,
+        routingKey: eventArgs.BasicProperties.ReplyTo,// response mesajının hangi kuyruğa gönderileceğini belirtiyoruz.
+        basicProperties: properties,
+        body: responseMessage
+        );
+};
+
+
+Console.Read();
+
+
---
+++ Request Response Example/RequestResponse.Publisher/Program.cs
@@ -0,0 +1,69 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+//bağlantı oluşturma
+ConnectionFactory factory = new();
+factory.Uri = new("");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+string requestQueueName = "example-request-response-queue";
+channel.QueueDeclare(
+    queue: requestQueueName,
+    durable: false,
+    exclusive: false,
+    autoDelete: false
+    );
+
+// Response queue oluşturuluyoruz
+string replyQueueName = channel.QueueDeclare().QueueName;
+
+
+string correlationId = Guid.NewGuid().ToString(); // correlationId oluşturuluyoruz. // Request ve Response mesajlarını eşleştirmek için kullanılır.
+
+
+#region Request Mesajını oluşturma Ve Gönderme
+IBasicProperties properties = channel.CreateBasicProperties();
+properties.CorrelationId = correlationId; // correlationId'yi ekliyoruz.// Göndereceğimiz mesajın hangi kuyruğa gönderileceğini belirtiyoruz.
+properties.ReplyTo = replyQueueName; // Response mesajının hangi kuyruğa gönderileceğini belirtiyoruz.
+
+for (int i = 0;i<100; i++)
+{
+    byte[] messageBody = Encoding.UTF8.GetBytes($"Hello World {i}");
+    channel.BasicPublish(
+        exchange:string.Empty,
+        routingKey:requestQueueName,
+        body:messageBody,
+        basicProperties:properties
+        );
+}
+#endregion
+
+#region Response Kuyruğu Dinleme
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(
+    queue:replyQueueName,
+    autoAck:true,
+    consumer:consumer
+    );
+
+consumer.Received += (sender, eventArgs) =>
+{
+    if (eventArgs.BasicProperties.CorrelationId == correlationId)
+    {
+        //..
+        byte[] responseBytes = eventArgs.Body.ToArray();
+        string response = Encoding.UTF8.GetString(responseBytes);
+        Console.WriteLine($"Response: {response}");
+    }
+};
+
+
+
+#endregion
+
+
+
+Console.Read();
