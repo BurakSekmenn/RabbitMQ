54fba99d8bfe0354ccfdde33f68ef5730b4c9ca1
Burak Sekmen <burakskmn07@gmail.com>
2024-08-18 20:10:01 +0300

    Publish Subscribe Example

---
+++ Publish Subscribe Example/Consumer/Program.cs
@@ -0,0 +1,40 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+//bağlantı oluşturma
+ConnectionFactory factory = new();
+factory.Uri = new("YouUrl");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+
+string exchangeName = "example-pub-sub-exchange";
+channel.ExchangeDeclare(
+    exchange: exchangeName,
+    type: ExchangeType.Fanout);
+
+string queuname = channel.QueueDeclare().QueueName;
+
+channel.QueueBind(
+    queue:queuname,
+    exchange :exchangeName,
+    routingKey:string.Empty
+    );
+
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(
+    queue:queuname,
+    autoAck:false,
+    consumer:consumer
+    );
+
+consumer.Received += (sender, e) =>
+{
+    byte[] body = e.Body.ToArray();
+    string message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"Gelen Mesaj: {message}");
+};
+
+Console.Read();
---
+++ Publish Subscribe Example/Publisher/Program.cs
@@ -0,0 +1,33 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+//bağlantı oluşturma
+ConnectionFactory factory = new();
+factory.Uri = new("YouUrl");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+
+string exchangeName = "example-pub-sub-exchange";
+channel.ExchangeDeclare(
+    exchange: exchangeName,
+    type:ExchangeType.Fanout);
+
+for (int i = 0; i < 100; i++)
+{
+    byte[] message = Encoding.UTF8.GetBytes("Merhaba "+ i);
+    channel.BasicPublish(
+    exchange: exchangeName,
+    routingKey: string.Empty,
+    body: message
+    );
+}
+
+
+
+
+
+
+Console.Read();
